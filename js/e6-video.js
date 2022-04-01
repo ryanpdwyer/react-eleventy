50d9bed5bf215ab37c9f71ba6aafc77ef11b18eb
Ryan Dwyer <ryanpdwyer@gmail.com>
2022-04-01 13:20:21 -0400

    Updated e6 vidoe

--- js/e6-video.js
+++ js/e6-video.js
@@ -60,6 +60,55 @@ function App() {
     </li>
     </ol>
     <p>Remember to apply this same sort of analysis in your video report for each stress applied to the system.</p>
+
+
+    <h3>Iron Thiocyanate Equilibrium</h3>
+
+    <p>Here's an example of a temperature change, using the iron thiocyanate reaction (E).</p>
+
+
+    <p>
+    Fe<sup>3+</sup>(aq)
+    + SCN<sup>-</sup>(aq) ⇌
+    FeSCN<sup>2+</sup>(aq)
+    </p>
+
+    <p>Remember the Fe<sup>3+</sup> solution was a pale yellow and when mixed with SCN<sup>-</sup>, the solution turned a dark red.</p>
+
+    
+    <p><img src="/img/iron-thiocyanate-temp.png" alt="Iron thiocyanate solution gets lights as temperature increases" style={{width:"100%", maxWidth:"500px"}}/></p>
+
+    <p>The following questions reason through the stress, response, and connection to observations for the increase in temperature.</p>
+
+    <ol>
+        <li>
+    <MCQ options={
+     [
+         {children: "Shifted left (towards reactants)", correct: true},
+     {children: "Shifted right (towards products)"},
+     {children: "No change"}
+    ]}
+    correctFeedback="Correct! Since the product is a red, the lighter color indicates the reaction shifted left toward the pale yellow reactants."
+    >
+        From the observations, which direction did the reaction shift when temperature was increased?
+    </MCQ>
+        </li>
+        <li>
+            <MCQ options={[{children: "Reactants"}, {children: "Products", correct: true}, {children: "Impossible to tell"}]}
+            correctFeedback='Correct! reactants ⇌ products + heat (↑) causes a shift left to use up added "product" (heat).'
+            >
+                Remember that for Le Chatelier principle, we can reason an increase in temperature as a stress of added heat (↑), and a decrease in temperature as a stress of removed heat (↓). Which side of the reaction should heat go on, given the answer to the previous problem?
+            </MCQ>
+        </li>
+        <li>
+            <MCQ options={[{children: "Endothermic"}, {children: "Exothermic", correct: true}]}
+            correctFeedback='Correct! Exothermic means heat is released as a "product" in the reaction (ΔH is negative).'
+            >
+                Based on your previous answer, is the reaction endothermic or exothermic?
+            </MCQ>
+        </li>
+    </ol>
+
     <h5>Questions or Problems?</h5>
     <p>If you have any questions, send me an email: <Obfuscate email="dwyerry@mountunion.edu"
                         headers={{
